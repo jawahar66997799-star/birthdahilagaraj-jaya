6ac9576f994c0fb695d8edbb44494ade445f7ae2
jawahar66997799-star <jawahar66997799@gmail.com>
2026-01-09 16:19:45 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+// Make sure you include Firebase SDK in verify.html as well
+const firebaseConfig = {
+  apiKey: "AIzaSyC4nqqYCqyNaDRyQgSuc4BQ25AGBe3C4c8",
+  authDomain: "prankvideo-d2a1b.firebaseapp.com",
+  projectId: "prankvideo-d2a1b",
+  storageBucket: "prankvideo-d2a1b.appspot.com",
+  messagingSenderId: "415496932602",
+  appId: "1:415496932602:web:f97f88ff4cb9295c37cd43"
+};
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
